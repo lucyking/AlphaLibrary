df78077d1d0fb5f04ebfdc2b58744f6d9baa3a69
lucyking <lucyking@users.noreply.github.com>
2016-07-02 16:19:06 +0800

    Update conf.py
    rm     'sphinx.ext.autodoc'

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,8 +46,9 @@ print "[>>>][Version:]",ls
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+
+#     'sphinx.ext.autodoc',
 extensions = [
-    'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
